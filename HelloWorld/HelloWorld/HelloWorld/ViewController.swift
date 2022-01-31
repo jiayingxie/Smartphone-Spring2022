f8ebdcd8edf8ad15ecc2620326255a6a64486bea
jiayingxie <xie.jiay@northeastern.edu>
2022-01-30 19:12:42 -0800

    Checking in Project for Hello World

---
+++ HelloWorld/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jiaying Xie on 1/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
