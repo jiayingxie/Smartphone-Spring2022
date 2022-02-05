8d0771b1127fe7ab3b3b005700e0ecf39dcd4af7
jiayingxie <xie.jiay@northeastern.edu>
2022-02-04 20:08:20 -0800

    initialization for food images

---
+++ FoodImages/FoodImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodImages
+//
+//  Created by Jiaying Xie on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tblView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
