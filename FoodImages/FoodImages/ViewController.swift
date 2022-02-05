67328e1749c804e597d04b6ed5f6834660164b7d
jiayingxie <xie.jiay@northeastern.edu>
2022-02-04 20:14:34 -0800

    initialization for food images

--- FoodImages/FoodImages/ViewController.swift
+++ FoodImages/FoodImages/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
