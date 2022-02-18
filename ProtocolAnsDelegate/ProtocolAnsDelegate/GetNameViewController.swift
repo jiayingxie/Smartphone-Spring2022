4a43b4cb49e061acb22d6356a7962ee44c0f3ab9
jiayingxie <xie.jiay@northeastern.edu>
2022-02-17 17:25:29 -0800

    Checking in code for example of protocol and delegate

---
+++ ProtocolAnsDelegate/ProtocolAnsDelegate/GetNameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GetNameViewController.swift
+//  ProtocolAnsDelegate
+//
+//  Created by Jiaying Xie on 2/13/22.
+//
+
+import UIKit
+
+protocol SendFirstAndLastNameDelegate {
+    func setFirstAndLastName(firstName: String, lastName: String)
+}
+
+class GetNameViewController: UIViewController {
+    var firstName : String?
+    var lastName : String?
+    
+    var sendFirstAndLastNameDelegate : SendFirstAndLastNameDelegate?
+    
+    @IBOutlet weak var txtFirstName: UITextField!
+    @IBOutlet weak var txtLastName: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let firstName = firstName else {return}
+        guard let lastName = lastName else {return}
+        
+        txtFirstName.text = firstName
+        txtLastName.text = lastName
+    }
+    
+    @IBAction func saveName(_ sender: Any) {
+        guard let first = txtFirstName.text else {return}
+        guard let last = txtLastName.text else {return}
+        
+        sendFirstAndLastNameDelegate?.setFirstAndLastName(firstName: first, lastName: last)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
---
+++ ProtocolAnsDelegate/ProtocolAnsDelegate/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  ProtocolAnsDelegate
+//
+//  Created by Jiaying Xie on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SendFirstAndLastNameDelegate {
+    
+    
+    
+    @IBOutlet weak var lblFirstName: UILabel!
+    
+    @IBOutlet weak var lblLastName: UILabel!
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getNameFromUser(_ sender: Any) {
+        performSegue(withIdentifier: "segueGetName", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueGetName" {
+            let getNameVC = segue.destination as! GetNameViewController
+            
+            guard let firstName = lblFirstName.text else {return}
+            guard let lastName = lblLastName.text else {return}
+            
+            getNameVC.firstName = firstName
+            getNameVC.lastName = lastName
+            
+            getNameVC.sendFirstAndLastNameDelegate = self
+            
+        }
+    }
+    
+    
+    func setFirstAndLastName(firstName: String, lastName: String) {
+        lblFirstName.text = firstName
+        lblLastName.text = lastName
+    }
+    
+    
+    
+}
+
